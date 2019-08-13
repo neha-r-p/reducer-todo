578a46cb0db17011a8bf6d91ebdf4063763db690
Neha Patel <neha.r.p@gmail.com>
2019-08-13 14:28:12 -0700

    reducer.js complete. hopefully

--- reducer-todo/src/reducers/reducer.js
+++ reducer-todo/src/reducers/reducer.js
@@ -1,31 +1,27 @@
-
-export const initialState = { todos: []}
+export const initialState = { todos: [] };
 
 export const Reducer = (state, action) => {
-
-    switch (action.type) {
-        case 'ADD_TODO':
-          return {
-            ...state,
-            todos: [ ...state.todos,
-              {item: action.payload,
-            completed: false,
-            id: Date.now() }
-            ]
-          };
-        case 'TOGGLE_TODO':
-          return {
-            ...state,
-            completed: !state.completed
-          };
-        case 'REMOVE_TODO':
-            return {
-                ...state,
-                state.todos.filter(todo => todo.completed === false)
-
-            }
-        default:
-          return state.todos;
-      }
-
-}
+  switch (action.type) {
+    case "ADD_TODO":
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          { item: action.payload, completed: false, id: Date.now() }
+        ]
+      };
+    case "TOGGLE_TODO":
+      return {
+        ...state,
+        todos: 
+          state.todos.map(todo => todo.id === action.id && !todo.completed)
+      };
+    case "REMOVE_TODO":
+      return {
+        ...state,
+        todos: state.todos.filter(todo => !todo.completed)
+      };
+    default:
+      return state;
+  }
+};
