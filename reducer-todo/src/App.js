8d51aef7c1dbec402ebf44e856e7170dc85c9a7c
Neha Patel <neha.r.p@gmail.com>
2019-08-13 13:47:57 -0700

    building out reducer

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
 import './App.css';
 import TodoForm from './components/TodoForm'
 import TodoList from './components/TodoList';
+import { initialState, Reducer }  from './reducers/reducer';
+
 
 function App() {
+  const [ todoList, setTodoList ] = useState();
+
   return (
     <div className="App">
       <TodoForm />
--- reducer-todo/src/reducers/reducer.js
+++ reducer-todo/src/reducers/reducer.js
@@ -1,6 +1,26 @@
 
-export const initialState = {[]}
+export const initialState = { todos: []}
 
 export const Reducer = (state, action) => {
+
+    switch (action.type) {
+        case 'ADD_TODO':
+          return {
+            ...state,
+            item: action.payload,
+          };
+        case 'TOGGLE_TODO':
+          return {
+            ...state,
+            completed: !state.completed
+          };
+        case 'REMOVE_TODO':
+            return {
+                ...state,
+
+            }
+        default:
+          return state;
+      }
 
 }
